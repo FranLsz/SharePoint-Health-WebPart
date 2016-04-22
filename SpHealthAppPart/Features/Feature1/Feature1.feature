6c1c6f321fce6f5757205caee6324b566d24fec2
FranLsz <franlsz95develop@gmail.com>
2016-04-22 11:55:00 +0200

    Final

---
+++ SpHealthAppPart/Features/Feature1/Feature1.feature
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="3dce50bf-60a4-4ef2-89d2-14783d3730dd" description="SharePoint Add-in Feature" featureId="3dce50bf-60a4-4ef2-89d2-14783d3730dd" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SpHealthAppPart Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e730b914-d27c-482d-9cef-bc975409ba05" />
+    <projectItemReference itemId="1c6317aa-add0-4d13-8e77-bf2c551646dd" />
+    <projectItemReference itemId="e49dcdf6-3dbc-4621-aa46-6e7d6fb5325a" />
+    <projectItemReference itemId="ea664687-d25a-412f-97fc-1f2798169784" />
+    <projectItemReference itemId="19d25e5e-79af-47c9-843e-f787fca3abde" />
+  </projectItems>
+</feature>
---
+++ SpHealthAppPart/Scripts/healthscores.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+//var Contoso = window.Contoso || {};
+
+//Contoso.HealthScores = function () {
+
+$('button').button();
+
+var healthScore;
+var pollInterval;
+var getHealthScore = function () {
+
+    $.ajax({
+
+        type: "HEAD",
+        url: "_layouts/15/blank.htm",
+
+        success: function (data, status, xhr) {
+            healthScore = xhr.getResponseHeader("X-SharePointHealthScore");
+            $('#healthPara').text("Server health score: " + healthScore);
+            $('#healthBar').progressbar("option", "value", parseInt(healthScore));
+        }
+    });
+
+};
+
+var startPolling = function () {
+
+    $('#healthBar').progressbar({
+        max: 10,
+        value: 0
+    });
+
+    getHealthScore();
+    pollInterval = setInterval(getHealthScore, 5000);
+};
+
+var stopPolling = function () {
+
+    clearInterval(pollInterval);
+    $('healthPara').text("Click Start Polling to poll the server for health scores.");
+    $('#healthBar').progressbar("destroy");
+};
+
+//return {
+//    StartPolling: startPolling,
+//    StopPolling: stopPolling
+//}
+
+//}();
